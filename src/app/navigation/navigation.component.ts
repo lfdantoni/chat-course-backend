1d3e749b93f9131c0033d0d7c806243223864d76
Leonardod <lfdantoni@gmail.com>
2017-05-21 21:48:09 -0300

    add menu mobile

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -10,6 +10,8 @@ import * as $ from 'jquery'
 export class NavigationComponent implements OnInit {
   sections: ISection[] = [{name: 'Home', active: true, hashTag: 'home'}, {name: 'Skills', active: false, hashTag: 'skills'}]
   activeSection: ISection;
+  selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
+  dropDownOpen: boolean = false;
 
   constructor() { 
 
@@ -27,7 +29,58 @@ export class NavigationComponent implements OnInit {
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    console.log(event);
+    if ($(event.target).width() > 767 && $('.navbar-toggle').is(':hidden')) {
+        $(this.selected).removeClass('slide-active');
+    }
+
+    // console.log($('.navbar-toggle'));
     console.log(event.target.innerWidth);
+  }
+
+  dropToggle($event){
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.dropDownOpen = !this.dropDownOpen;
+  }
+
+  toggleMenu($event){
+    $event.stopPropagation();
+    $event.preventDefault();
+
+    //stick in the fixed 100% height behind the navbar but don't wrap it
+    $('#slide-nav.navbar .container').append($('<div id="navbar-height-col"></div>'));
+
+    // Enter your ids or classes
+    var toggler = '.navbar-toggle';
+    var pagewrapper = '#page-content';
+    var navigationwrapper = '.navbar-header';
+    var menuwidth = '100%'; // the menu inside the slide menu itself
+    var slidewidth = '80%';
+    var menuneg = '-100%';
+    var slideneg = '-80%';
+    var selected = $($event.currentTarget).hasClass('slide-active');
+
+    $('#slidemenu').stop().animate({
+        left: selected ? menuneg : '0px'
+    });
+
+    $('#navbar-height-col').stop().animate({
+        left: selected ? slideneg : '0px'
+    });
+
+    $(pagewrapper).stop().animate({
+        left: selected ? '0px' : slidewidth
+    });
+
+    $(navigationwrapper).stop().animate({
+        left: selected ? '0px' : slidewidth
+    });
+
+
+    $($event.currentTarget).toggleClass('slide-active', !selected);
+    $('#slidemenu').toggleClass('slide-active');
+
+
+    $('#page-content, .navbar, body, .navbar-header').toggleClass('slide-active');
   }
 }
