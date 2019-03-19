89016b6e179c8d92dc3b6a78e73183a5072cc958
Leonardod <lfdantoni@gmail.com>
2019-03-18 22:40:25 -0300

    make it better

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+let clients = 0;
+wss.on('connection', (ws) => {
+    clients++;
+  console.log('Client '+ clients +' connected');
+  ws.on('close', () => {
+    clients--;
+      console.log('Client disconnected');
+  });
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString() + 'clients: ' + clients);
+  });
+}, 1000);
